9b856b5b1057a8e307e3f2631ca076ddd63b2a86
Maciej Duda <mdud1999@gmail.com>
2021-08-25 15:08:00 +0200

    database saves changes

--- rest-service/src/main/java/com/example/restservice/material/MaterialConfig.java
+++ rest-service/src/main/java/com/example/restservice/material/MaterialConfig.java
@@ -9,31 +9,31 @@ import java.util.List;
 
 @Configuration
 public class MaterialConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(MaterialRepository repository){
-        return args -> {
-            Material gumka = new Material(
-                    "Gumka",
-                    20,
-                    5.00
-            );
-            Material pasek = new Material(
-                    "Pasek",
-                    15,
-                    7.50
-            );
-            Material koronka= new Material(
-                    "Koronka",
-                    40,
-                    3.50
-            );
-            List<Material> tempList = new ArrayList<Material>();
-            tempList.add(gumka);
-            tempList.add(pasek);
-            tempList.add(koronka);
-            repository.saveAll(
-                    tempList
-            );
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(MaterialRepository repository){
+//        return args -> {
+//            Material gumka = new Material(
+//                    "Gumka",
+//                    20,
+//                    5.00
+//            );
+//            Material pasek = new Material(
+//                    "Pasek",
+//                    15,
+//                    7.50
+//            );
+//            Material koronka= new Material(
+//                    "Koronka",
+//                    40,
+//                    3.50
+//            );
+//            List<Material> tempList = new ArrayList<Material>();
+//            tempList.add(gumka);
+//            tempList.add(pasek);
+//            tempList.add(koronka);
+//            repository.saveAll(
+//                    tempList
+//            );
+//        };
+//    }
 }
