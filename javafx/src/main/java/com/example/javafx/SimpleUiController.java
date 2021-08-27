08885bb8c4febc40f3b4cd166c982b37f996a058
Maciej Duda <mdud1999@gmail.com>
2021-08-27 09:30:56 +0200

    connection label and button

--- javafx/src/main/java/com/example/javafx/SimpleUiController.java
+++ javafx/src/main/java/com/example/javafx/SimpleUiController.java
@@ -4,16 +4,20 @@ import javafx.application.HostServices;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 @Component
-public class SimpleUiController {
+public class SimpleUiController implements Initializable {
     private JsonGetter jsonGetter = new JsonGetter();
     private HttpRequester httpRequester = new HttpRequester();
     private final HostServices hostServices;
@@ -33,6 +37,9 @@ public class SimpleUiController {
     @FXML
     public TextField filterMaterial;
 
+    @FXML
+    public Label connectionLabel;
+
     SimpleUiController(HostServices hostServices) {
         this.hostServices = hostServices;
     }
@@ -46,16 +53,17 @@ public class SimpleUiController {
     @FXML
     public void connectJson(){
         try {
-            //jsonGetter.getJson();
             ObservableList<Material> helpList = FXCollections.observableList(jsonGetter.getJson());
             testList.setItems(helpList);
-            //jsonGetter.deleteElement(8);
-            //httpRequester.deleteRequest(8);
-            //httpRequester.editRequest(new Material(4,"Test material", 5, 7.20));
-            //httpRequester.addRequest(new Material("nowy material",69,420.0));
+            connectionLabel.setTextFill(Color.GREEN);
+            connectionLabel.setText("Połączono");
         } catch (IOException e) {
+            connectionLabel.setTextFill(Color.RED);
+            connectionLabel.setText("Wystąpił błąd z połączeniem");
             e.printStackTrace();
         } catch (InterruptedException e) {
+            connectionLabel.setTextFill(Color.RED);
+            connectionLabel.setText("Wystąpił błąd bazy danych");
             e.printStackTrace();
         }
     }
@@ -63,4 +71,32 @@ public class SimpleUiController {
     public void filterTable(){
 
     }
+
+    @Override
+    public void initialize (URL url, ResourceBundle rb){
+        try {
+            ObservableList<Material> helpList = FXCollections.observableList(jsonGetter.getJson());
+            testList.setItems(helpList);
+            connectionLabel.setTextFill(Color.GREEN);
+            connectionLabel.setText("Połączono");
+        } catch (IOException e) {
+            connectionLabel.setTextFill(Color.RED);
+            connectionLabel.setText("Wystąpił błąd z połączeniem");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            connectionLabel.setTextFill(Color.RED);
+            connectionLabel.setText("Wystąpił błąd bazy danych");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public Label nameLabel;
+
+    @FXML
+    public Label quantityLabel;
+
+    @FXML
+    public Label priceLabel;
+
 }
